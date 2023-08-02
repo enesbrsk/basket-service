53957f4e3be8f210754c17c8fe5b2429cf5dd64b
enesbrsk <enesbrsk@hotmail.com>
2023-08-02 12:32:21 +0300

    data up

---
+++ src/main/java/com/service/englishdise/EnglishdiseApplication.java
@@ -0,0 +1,13 @@
+package com.service.englishdise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishdiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishdiseApplication.class, args);
+	}
+
+}
